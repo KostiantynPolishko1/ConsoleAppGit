92fc8c531ed92c015e380af8529e0e3bc0260b4e
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-03-31 22:35:48 +0300

    1 created class Person

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit
+{
+    public class Person
+    {
+        public int age {  get; private set; }
+        public string? sex { get; private set; }
+
+        public Person() : this (18, null) { }
+
+        public Person(int age, string? sex)
+        {
+            this.age = age;
+            this.sex = sex??= "gender";
+        }
+
+        public override string ToString() => $"age : {age}, sex : {sex}"; 
+    }
+}
